d207f5d634e0efb519b5abb06b0849e2ffc21893
batu1579 <51915056+batu1579@users.noreply.github.com>
2022-11-22 14:17:01 +0800

    🔧 build: 更新模板到 v2.5.3

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@
  * @Author: BATU1579
  * @CreateDate: 2022-05-24 16:58:03
  * @LastEditor: BATU1579
- * @LastTime: 2022-09-23 17:45:32
+ * @LastTime: 2022-11-20 20:07:04
  * @FilePath: \\src\\index.ts
  * @Description: 脚本入口
  */
