ebd5882a41beba0b03841d8b79053399aba6ea04
batu1579 <51915056+batu1579@users.noreply.github.com>
2022-11-22 14:17:42 +0800

    🔧 build: 更新类型声明

--- hamibot-types/types/ocr.d.ts
+++ hamibot-types/types/ocr.d.ts
@@ -2,12 +2,12 @@
  * @Author: BATU1579
  * @CreateDate: 2022-06-03 01:16:42
  * @LastEditor: BATU1579
- * @LastTime: 2022-08-16 03:08:49
- * @FilePath: \\src\\types\\ocr.d.ts
+ * @LastTime: 2022-10-25 15:41:43
+ * @FilePath: \\types\\ocr.d.ts
  * @Description: 文字识别模块
  */
 declare module 'ocr' {
-    import { Image } from "images";
+    import { BaseImage } from 'images';
 
     global {
         /**
@@ -20,7 +20,7 @@ declare module 'ocr' {
     interface OCR {
         /**
          * @description: 识别图片中的文字。
-         * @param {Image} img 要识别的图片。
+         * @param {BaseImage} img 要识别的图片。
          * @return {OcrResult} OCR 识别结果。
          * @since 1.2.2
          * @example
@@ -34,11 +34,11 @@ declare module 'ocr' {
          * log(res);
          * ```
          */
-        recognize(img: Image): OcrResult;
+        recognize(img: BaseImage): OcrResult;
 
         /**
-         * @description: 识别图片中的文字，只返回文本结果。
-         * @param {Image} img 要识别的图片。
+         * @description: 识别图片中的文字，只返回完整文本结果。
+         * @param {BaseImage} img 要识别的图片。
          * @return {OcrResult} OCR 识别结果。
          * @since 1.2.2
          * @example
@@ -52,11 +52,47 @@ declare module 'ocr' {
          * log(res);
          * ```
          */
-        recognizeText(img: Image): OcrResult;
+        recognizeText(img: BaseImage): string;
     }
 
-    // TODO: 补全属性和方法
     interface OcrResult {
-        [prop: string]: any;
+        /**
+         * @description: 识别结果组合成的完整结果
+         */
+        text: string;
+
+        /**
+         * @description: 全部识别结果
+         */
+        results: OcrResultDetail[];
+
+        /**
+         * @description: 识别用时，单位毫秒。
+         */
+        elapsed: number;
+    }
+
+    interface OcrResultDetail {
+        /**
+         * @description: 识别结果的边缘坐标。
+         */
+        bounds: OcrResultBounds;
+
+        /**
+         * @description: 结果的置信度。
+         */
+        confidence: number;
+
+        /**
+         * @description: 识别结果。
+         */
+        text: string;
+    }
+
+    interface OcrResultBounds {
+        bottom: number;
+        left: number;
+        right: number;
+        top: number;
     }
 }
